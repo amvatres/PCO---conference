c2b90caf2bf8d0f9908366638b863b9b2022c384
amela.vatre@stu.ibu.edu.ba <amela.vatres@stu.ibu.edu.ba>
2019-09-05 23:33:13 +0200

    Live db

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
     'secret': 'ProfessionalConferenceOrganizer##1337##',
     'database': 'mongodb://localhost:27017/ConferenceOrganizer',
+ 'livedb': 'mongodb://AMXYVA:ConferenceOrganizer@cluster0-shard-00-00-7bg7v.mongodb.net:27017,cluster0-shard-00-01-7bg7v.mongodb.net:27017,cluster0-shard-00-02-7bg7v.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
 /*     'livedb': 'mongodb://amvatres:seproject123@ds033170.mlab.com:33170/employeetracker'
  */
 };
